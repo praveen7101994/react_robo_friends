8afd38a62eb93b989c387d41ec75a4f1978648bd
Praveen Patel <30315923+praveen7101994@users.noreply.github.com>
2019-05-16 20:35:37 +0530

    uploaded final project
    uploaded final robofriends project

---
+++ precache-manifest.73592fef23917d36ee71e9591bdd4608.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8c47a511e87ae9b0d3a4",
+    "url": "/static/js/main.4a5d676b.chunk.js"
+  },
+  {
+    "revision": "98d555baddec17377c04",
+    "url": "/static/js/2.c92d2834.chunk.js"
+  },
+  {
+    "revision": "8c47a511e87ae9b0d3a4",
+    "url": "/static/css/main.b2f53a0b.chunk.css"
+  },
+  {
+    "revision": "98d555baddec17377c04",
+    "url": "/static/css/2.2fbdca75.chunk.css"
+  },
+  {
+    "revision": "cf72963c6e8cf27a31aa7e70b57fea0c",
+    "url": "/index.html"
+  }
+];
